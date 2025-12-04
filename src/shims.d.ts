05bdcadff29102eb8cab554c77c2aa17a052da95
Mike Farrow <mike@farrow.io>
2025-12-04 13:39:03 +0000

    removing temp shims

--- src/shims.d.ts
+++
@@ -1,7 +0,0 @@
-import { IntrinsicDefinitions as SanityTypesIntrinsicDefinitions } from '@sanity/types';
-
-/* Fix for types not being inferred in defineField */
-declare module 'sanity' {
-  // eslint-disable-next-line typescript/no-empty-object-type
-  interface IntrinsicDefinitions extends SanityTypesIntrinsicDefinitions {}
-}
